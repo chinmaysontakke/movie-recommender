e3c2cfeefd779a26a2509e4888457759919e7332
chinmaysontakke <141616084+chinmaysontakke@users.noreply.github.com>
2025-04-26 20:41:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -35,7 +35,7 @@ def recommend(movie):
 # Load Data
 # ---------------------------------
 
-movies = pickle.load(open('model/movies_dict.pkl', 'rb'))
+movies = pickle.load(open('model/movie_dict.pkl', 'rb'))
 
 # ---------------------------------
 # User Management
