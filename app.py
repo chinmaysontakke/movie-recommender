e75d334546704304f186832ca5066c8108fc1ab2
chinmaysontakke <141616084+chinmaysontakke@users.noreply.github.com>
2025-04-27 15:30:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -93,7 +93,7 @@ st.markdown(
     }
     </style>
     <video autoplay muted loop id="background-video">
-        <source src="YOUR_VIDEO_URL.mp4" type="video/mp4">
+        <source src="holly.mp4" type="video/mp4">
         Your browser does not support HTML5 video.
     </video>
     """,
